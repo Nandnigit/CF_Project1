a10a58e22193cadfce3c53921210d53c23d146e4
Nandini <nandinigupta63696@gmail.com>
2024-06-12 13:19:06 +0530

    push8

--- src/Components/SignUp/Signup.jsx
+++ src/Components/SignUp/Signup.jsx
@@ -87,8 +87,9 @@ function Signup() {
           role === 'student' ? 'https://novajobs.us/api/students/countries' : 'https://novajobs.us/api/trainers/countries'
         );
         console.log("res",response.data.data );
-        setCountries(Array.isArray(response.data) ? response.data : []);
+        setCountries(Array.isArray(response.data) ? response.data.data : []);
         console.log(countries);
+        console.log(countries.id)
       } catch (error) {
         console.error('Error fetching countries:', error);
       }
